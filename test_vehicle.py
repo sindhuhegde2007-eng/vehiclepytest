18b001a88d2533f7997f12e953a4811b0416150f
sindhu hegde <sindhuhegde2007@gmail.com>
2025-12-02 12:04:43 +0530

    Fix formatting of expected output in tests

--- test_vehicle.py
+++ test_vehicle.py
@@ -1,10 +1,12 @@
 from vehicle import vehicle_registration
 
 def test_vehicle_registration():
-    expected_output=(
-      Vehicle number="KA31N2881"
-   Vehicle owner name="sara"
-    Vehicle type="car"
-    Year of manifacture=2015
+    expected_output = (
+        "Vehicle number:KA31N2881\n"
+        "vehicle owner name:sara\n"
+        "vehicle type:car\n"
+        "year of manufacture:2015"
     )
-    assert vehicle_registration("KA31N2881","sara","car",2015)==expected_output
+
+    assert vehicle_registration("KA31N2881", "sara", "car", 2015) == expected_output
+
