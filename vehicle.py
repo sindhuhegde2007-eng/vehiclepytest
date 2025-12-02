960f6d390d1d5d6148ceba4efb01f3cc8ef1304d
sindhu hegde <sindhuhegde2007@gmail.com>
2025-12-02 11:42:19 +0530

    Add vehicle registration function in vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,15 @@
+import pytest
+def vehicle_registration(number,owner_name,vehicle_type,manifacture):
+    result=(
+        f"Vehicle number:{number}\n"
+        f"vehicle owner name:{vehicle_name}\n"
+        f"vehicle type:{vehicle_type}\n"
+        f"year of manufacture:{manifacture}"
+    )
+    return result
+if __name__=="__main__":
+    number="KA31N2881"
+   owner_name="sara"
+    vehicle_type="car"
+    manifacture=2015
+    print(vehicle_registration(number,owner_name,vehicle_type,manifacture))
