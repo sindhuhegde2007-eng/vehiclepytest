42fb975f18c14212879c0ce99381752d0e97a8f3
sindhu hegde <sindhuhegde2007@gmail.com>
2025-12-02 12:01:22 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,15 +1,17 @@
 import pytest
-def vehicle_registration(number,owner_name,vehicle_type,manifacture):
-    result=(
+
+def vehicle_registration(number, name, vehicle_type, manifacture):
+    result = (
         f"Vehicle number:{number}\n"
-        f"vehicle owner name:{owner_name}\n"
+        f"vehicle owner name:{name}\n"
         f"vehicle type:{vehicle_type}\n"
         f"year of manufacture:{manifacture}"
     )
     return result
-if __name__=="__main__":
-    number="KA31N2881"
-   owner_name="sara"
-    vehicle_type="car"
-    manifacture=2015
-    print(vehicle_registration(number,owner_name,vehicle_type,manifacture))
+
+if __name__ == "__main__":
+    number = "KA31N2881"
+    name = "sara"
+    vehicle_type = "car"
+    manifacture = 2015
+    print(vehicle_registration(number, name, vehicle_type, manifacture))
